12ab43eddde7d8a47f11937c8198f53fe630a6e3
Alon Shraybman <alons@college.labs>
2025-12-17 11:45:25 +0200

    starting task from manager

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+import {add,substrack,mulitiply,devied} from './t1.js'
 console.log("hello world")
+console.log("what is your name")
+console.log(add(5,3))
+console.log(devied(5,3))
+console.log(substrack(5,3))
+console.log(mulitiply(5,3))
