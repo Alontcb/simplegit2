0793a24ed5882d979776e52c642b55fe4d15e65c
Chaim Malov <ChaimMalov@gmail.com>
2025-12-17 12:59:23 +0200

    seven Boom

--- t1.js
+++ t1.js
@@ -1,4 +1,5 @@
-export const add = (x,y) =>x+y
-export const substrack = (x,y) =>x-y
-export const mulitiply = (x,y) =>x*y
-export const devied = (x,y) =>y==0?null:x/y
+export const add = (x,y) =>x + y
+export const substrack = (x,y) =>x - y
+export const multiply = (x,y) =>x * y
+export const devied = (x,y) =>y == 0 ? null : x / y
+export const abs = (x) => x < 0 ? -x : x
